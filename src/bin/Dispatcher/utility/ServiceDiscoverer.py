170ea51fc99db715c81e48a16e61be92474877f8
Krzysztof Kruk <krzysztof.pawel.kruk@gmail.com>
2016-07-24 21:56:48 +0200

    * Fixed bug on empty regex in ServiceDiscoverer

--- src/bin/Dispatcher/utility/ServiceDiscoverer.py
+++ src/bin/Dispatcher/utility/ServiceDiscoverer.py
@@ -2,6 +2,8 @@ from serial.tools import list_ports
 
 
 def get_port_name(regex):
+    if not regex:
+        return None
     for device_info in list_ports.grep(regex):
         return device_info.device
     else:
@@ -9,4 +11,4 @@ def get_port_name(regex):
 
 
 if __name__ == "__main__":
-    print(get_port_name("/dev/ttyUSB0"))
+    print(get_port_name(""))
