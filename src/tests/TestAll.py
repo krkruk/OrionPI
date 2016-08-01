7fb1c09bb3324c2bae6102a4a473db91d4669931
Krzysztof Kruk <krzysztof.pawel.kruk@gmail.com>
2016-08-01 09:39:50 +0200

    * Added TestAll in tests - runs all tests

---
+++ src/tests/TestAll.py
@@ -0,0 +1,14 @@
+from tests.Dispatcher.TestDispatchingSystem import *
+from tests.Dispatcher.Devices.TestDeviceManagerFactory import *
+from tests.Dispatcher.Devices.Manipulator.TestManipulatorFactory import *
+from tests.Dispatcher.Devices.Manipulator.TestManipulatorJSONTranslator import *
+from tests.Dispatcher.Devices.Propulsion.TestPropulsionFactory import *
+from tests.Dispatcher.Settings.TestSettings import *
+from tests.Dispatcher.Settings.TestSettingsManager import *
+from tests.Dispatcher.utility.TestLineReader import *
+from tests.Dispatcher.utility.TestLineWriter import *
+import unittest
+
+
+if __name__ == "__main__":
+    unittest.main()
